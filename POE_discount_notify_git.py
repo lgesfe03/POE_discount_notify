61c65215d6d3fac76c6b108c91d5c261fff78c97
Mark Lin <94594384+lgesfe03@users.noreply.github.com>
2024-01-31 23:49:01 +0800

    scroll down until bottom

--- POE_discount_notify_git.py
+++ POE_discount_notify_git.py
@@ -1,14 +1,16 @@
+import time
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 look = "Invisible Buff Effect"
-# test
-# look = "Scavenger Ratmother Pet"
-# look = "Midnight Pact Character Effect"
 url = "https://www.pathofexile.com/shop/category/specials"
+# test
+# look = "Gore Stormbind Effect"
+# url = "https://www.pathofexile.com/shop/category/alternate-skill-effects"
 LINE_url = 'https://notify-api.line.me/api/notify'
-LINE_token = '' #regist at https://notify-bot.line.me/zh_TW/
+LINE_token = 'vunr6lJWO9OcwAT08caWQ8s1RRNjZrpA9ErRypGEOiN' #regist at https://notify-bot.line.me/zh_TW/
 LINE_headers = {'Authorization': 'Bearer ' + LINE_token}
 Content = ""
 # Use Chrome as the browser
@@ -17,6 +19,22 @@ is_exist = False
 # Load the webpage
 browser.get(url)
 
+# Scroll down the page
+# Get the initial scroll position
+prev_scroll_position = browser.execute_script("return window.pageYOffset;")
+# Scroll down the page
+browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
+while True:
+    # Wait for the page to load
+    time.sleep(1)
+    # Get the current scroll position
+    curr_scroll_position = browser.execute_script("return window.pageYOffset;")
+    if curr_scroll_position == prev_scroll_position:
+        # Reached the bottom of the page
+        break
+    # Scroll down again
+    browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
+    prev_scroll_position = curr_scroll_position
 # Find all elements with class="shopItem-container"
 shop_items = browser.find_elements(By.CLASS_NAME, "shopItem-container")
 
